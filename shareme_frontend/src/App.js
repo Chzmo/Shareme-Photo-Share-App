c2603484a9ec5d59197581d8c5ef0780efb8a963
chzmo <chisomomoyo10@gmail.com>
2022-10-22 09:34:08 +0200

    Project Setup

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,57 +1,10 @@
 import {useEffect, useState} from "react";
-import './App.css'
-import SearchIcon from './search.svg'
-import MovieCard from "./MovieCard";
-
-const API_URL = 'http://www.omdbapi.com/?apikey=bc183683'
 
 const App = () =>{
-    const [movies, SetMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const searchMovies = async(title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-
-        SetMovies(data.Search);
-    }
-
-    useEffect(() => {
-        searchMovies('spiderman')
-    }, []);
 
     return (
         <div className="app">
             <h1>MovieLand</h1>
-            <div className="search">
-                <input 
-                    placeholder="Search for Movies"
-                    value = {searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    type="text" 
-                />
-
-                <img 
-                    src={SearchIcon} 
-                    alt="search"
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-
-            {
-                movies.length > 0 ?
-                (
-                    <div className="container">
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie}/>
-                        ))}
-                    </div>
-                ):(
-                    <div className="empty">
-                        <h2>No movies found</h2>
-                    </div>
-                )
-            }
         </div>
     );
 }
