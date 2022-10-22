29af3fc2e993713d770c8046f160305c004d40f2
chzmo <chisomomoyo10@gmail.com>
2022-10-22 10:54:15 +0200

    Project Setup

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -3,10 +3,10 @@ import React from 'react'
 const App = () =>{
 
     return (
-        <div className="app">
-            <h1>MovieLand</h1>
-        </div>
-    );
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      )
 }
 
 export default App;
