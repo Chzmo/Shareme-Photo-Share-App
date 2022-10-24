13ab3e388d631bafdf076b19e628e02b7a5501cf
Chzmo <chisomomoyo10@gmail.com>
2022-10-24 14:46:34 +0200

    Working on login

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -7,7 +7,24 @@ import logo from '../assets/logowhite.png'
 
 function Login() {
   return (
-    <div className='flex justify-start items-center flex-col h-screen'>Login</div>
+    <div className='flex justify-start items-center flex-col h-screen'>
+        <div className="relative w-full h-full">
+            <video 
+                src={shareVideo}
+                type="video/mp4"
+                loop
+                controls = {false}
+                muted
+                autoPlay
+                className='w-full h-full object-cover'
+            />
+            <div className="absolute flex flex-col justify-center items-center left-0 right-0 top-0 bottom-0  bg-blackOverlay">
+                <div className="p-5">
+                    <img src={logo} alt="logo" width="130px"/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
