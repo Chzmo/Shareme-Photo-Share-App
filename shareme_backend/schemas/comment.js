534bfdc0cd213b9c071848cf693678750f3c1e37
Chzmo <chisomomoyo10@gmail.com>
2022-10-26 16:06:48 +0200

    Working on Backend

--- shareme_backend/schemas/comment.js
+++ shareme_backend/schemas/comment.js
@@ -2,4 +2,16 @@ export default{
     name: 'comment',
     title: 'Comment',
     type: 'document',
+    fields:[
+        {
+            name:'postedBy',
+            title:'PostedBy',
+            type:'postedBy'
+        },
+        {
+            name: 'comment',
+            title:'Comment',
+            type:'string'
+        }
+    ]
 }
