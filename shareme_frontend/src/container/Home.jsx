2feda2e2898cf682c8153ce0ea22f672798a353b
Chzmo <chisomomoyo10@gmail.com>
2022-10-28 15:16:39 +0200

    Working on Home

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -8,20 +8,38 @@ import Pins from './Pins';
 import { client } from '../client';
 import logo from '../assets/logo.png';
 
+import {userQuery} from '../utils/data'
+
 function Home() {
   const [toggleSidebar, setToggleSidebar] = useState(false);
-
+  const [user, setUser] = useState(null)
+  const UserInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  
+  useEffect(() => {
+    const query = userQuery(UserInfo?.googleId);
+    client.fetch(query)
+      .then((data)=>{
+        setUser(data[0]);
+      })
+  }, []);
+  
+  
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className="hidden md:flex hscreen flex-initial">
         <Sidebar/>
       </div>
       <div className="flex md:hidden flex-row">
-        <HiMenu fontFamily={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
         <Link to="/">
           <img src={logo} alt="logo" className="w-28"/>
         </Link>
-        </div>
+
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={logo} alt="logo" className="w-28"/>
+        </Link>
+
+      </div>
     </div>
   )
 }
