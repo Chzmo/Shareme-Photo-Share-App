731ce02e17dcd719f5b22bad42610339d8afce6f
Chzmo <chisomomoyo10@gmail.com>
2022-11-07 16:43:26 +0200

    connecting to database

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -3,11 +3,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import user from './user'
-import pin from './pin'
-import comment from './comment'
-import postedBy from './postedBy'
-import save from './save'   
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -16,6 +12,5 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    user, pin, comment, postedBy, save
   ]),
 })
