bc777024e7a3b8ecb6ec20a63a4094bec951064a
Chzmo <chisomomoyo10@gmail.com>
2022-11-09 16:32:40 +0200

    working on feed section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+
+import {client} from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner'
+
 
 function Feed() {
   return (
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div>Spinner</div>
+  )
+}
+
+export default Spinner
