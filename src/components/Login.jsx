21ed2750bda2df4b877275bf2a13cace98aa1f2e
Chzmo <chisomomoyo10@gmail.com>
2022-11-09 11:23:39 +0200

    Implementing home page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,26 +17,6 @@ function Login() {
         })
     })
 
-    // const mutations = [{
-    //     createOrReplace: {
-    //       _id: '123',
-    //       _type: 'cms.article',
-    //       title: 'An article'
-    //     }
-    //   }]
-    
-    //   fetch(`https://${projectId}.api.sanity.io/v2021-06-07/data/mutate/${datasetName}`, {
-    //     method: 'post',
-    //     headers: {
-    //         'Content-type': 'application/json',
-    //         Authorization: `Bearer ${tokenWithWriteAccess}`
-    //     },
-    //     body: JSON.stringify({mutations})
-    //     })
-    //     .then(response => response.json())
-    //     .then(result => console.log(result))
-    //     .catch(error => console.error(error))
-
     const responseGoogle = (response)=>{       
         localStorage.setItem('user', JSON.stringify(response.profileObj));
         const {name, googleId, imageUrl} = response.profileObj; 
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { HiMenu } from 'react-icons/hi';
-import { aiFillCloseCircle } from 'react-icons/ai';
+import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
 
 import { Sidebar, UserProfile } from '../components/index';
 import Pins from './Pins';
 import { client } from '../client';
 import logo from '../assets/logo.png';
+import avatar from  '../assets/logo-placeholder.png'
 
 import {userQuery} from '../utils/data'
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  const scrollRef = useRef(null);
   const UserInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
   
   
@@ -23,24 +25,45 @@ const Home = () => {
         setUser(data[0]);
       })
   }, []);
+
+  useEffect(() =>{
+    scrollRef.current.scrollTo(0, 0);
+  }, [])
   
-  console.log(user);
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className="hidden md:flex hscreen flex-initial">
-        <Sidebar/>
+        <Sidebar user={user && user}/>
       </div>
       <div className="flex md:hidden flex-row">
-        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28"/>
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          
-        </Link>
+        <div className="p-2 w-full flex flex row justify-between items-center shadow-md">
+          <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)}/>
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28"/>
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={avatar} alt="logo" className='w-28 h-28'/>
+          </Link>
+        </div>
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-shadow z-10 animate-slide-in">
+            <div className='absolute w-full flex justify-end'>
+              <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+            </div>
+            <Sidebar user={user && user} closedToggle={setToggleSidebar}/>
+          </div>
+        )}
+      </div>
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          <Route path="/user-progile/:userId" element={<UserProfile />}/>
+          <Route path="/" element={<Pins user = {user && user}/>}/>
+        </Routes>
       </div>
     </div>
   )
 }
 
+
 export default Home
+
