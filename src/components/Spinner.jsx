193743d769c21bad453c8bf2b12585dddf953432
Chzmo <chisomomoyo10@gmail.com>
2022-11-10 11:09:58 +0200

    added .env.example

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-// import Loader from 'react-loader-spinner'
+import React from 'react';
+import { Circles } from 'react-loader-spinner';
 
 function Spinner({message}) {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      {/* <Loader
+      <Circles
         type = "Circles"
         color = "#00BFFF"
         hieght={50}
-        width={20}
+        width={50}
         className="m-5"
-      /> */}
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
       <p className='text-lg text-center px--2'>
         {message}
       </p>
