ae184489abdb490ae0e0b5f9c7df4870a29ff88f
Chzmo <chisomomoyo10@gmail.com>
2022-11-15 16:54:12 +0200

    working on PinDetails

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {MdDownloadOffline} from 'react-icons/md'
+import {MdDownloadForOffline} from 'react-icons/md'
 import {Link, useParams} from 'react-router-dom'
 import {v4 as uuidv4} from 'uuid'
 
@@ -37,16 +37,40 @@ function PinDetail() {
     fetchPinDetails();
   },[pinId])
 
-  if(!pinDetail) return <Spinner message="Loading pins"/>
+  if(!pinDetail) return <Spinner message="Loading pin"/>
 
   return (
-    <div className="flex xl:flex-row flex-col m-auto bg-white" style={{maxWidth:'1500px', borderRadius:'32px'}}>
-      <div className="flex justify-center items-center md:items-start flex-initial">
+    <div className="flex flex-col m-auto bg-white" style={{maxWidth:'700px', borderRadius:'32px'}}>
+      <div className="flex justify-center items-center ">
         <img
           src={pinDetail?.image && urlFor(pinDetail.image).url()}
           className="rounded-t-3xl rounded-b-lg"
           alt='user-post'
+         
         />
+      </div>
+      <div className="w-full p-5 flex-1 xl:min-w-620">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2 items-center">
+            <a 
+              href={`${pinDetail.image?.asset?.url}?dl=`}
+              download
+              onClick={
+                (e)=> {e.stopPropagation()}
+              }
+              className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md hover:outline-none"
+            >
+              <MdDownloadForOffline />
+            </a>
+          </div>
+          <a 
+            href={pinDetail.destination} 
+            target="_blank" 
+            rel="noreferrer"
+          >
+            {pinDetail.destination} 
+          </a>
+        </div>
       </div>
     </div>
   )
