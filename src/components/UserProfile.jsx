75dff2ff5b80e6248d91db24c548eb2cca26ceae
Chzmo <chisomomoyo10@gmail.com>
2022-11-21 16:29:15 +0200

    Working on UserProfile

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {AiOutlineLogout} from 'react-icons/ai'
 import {useNavigate, useParams} from 'react-router-dom'
-import {GoogleLogout, GooglrLogout} from 'react-google-login'
+import {GoogleLogout} from 'react-google-login'
 
 import{userCreatedPinsQuery, userQuery, userSavedPinsQuery} from './../utils/data'
 import {client} from '../client'
@@ -9,10 +9,12 @@ import MasonryLayout from './MasonryLayout'
 import Spinner from './Spinner'
 
 const randomImage = "https://source.unsplash.com/1600x900/?programming/nature,photography,technology"
+const activeBtnStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
+const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none';
 
 function UserProfile() {
   const [user, setUser] = useState(null);
-  const [pin, setPin] = useState(null);
+  const [pins, setPins] = useState(null);
   const [text, setText] = useState('Created');
   const [activeBtn, setActiveBtn] = useState('created');
   
@@ -31,6 +33,26 @@ function UserProfile() {
 
   }, [userId])
 
+  useEffect(()=>{
+    if(text === "created"){
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+
+      client.fetch(createdPinsQuery)
+        .then((data) => {
+          setPins(data);
+        })
+    }else{
+      const saveddPinsQuery = userSavedPinsQuery(userId);
+
+      client.fetch(saveddPinsQuery)
+        .then((data) => {
+          setPins(data);
+        })
+    }
+
+  }, [text, userId])
+
+  
   if (!user){
     return <Spinner message={`Loading Profile...`} />
   }
@@ -59,7 +81,7 @@ function UserProfile() {
               {user?.userName}
             </h1>
             <div className='absolute top-0 z-1 right-0 p-2'>
-              {userId === user?._id && (
+              {(userId === user?._id && userId !== null) && (
                 <GoogleLogout
                   clientId = {clientId}
                   render={(renderProps)=>(
@@ -77,7 +99,31 @@ function UserProfile() {
               )}
             </div>
           </div>
-          <div className="tex-center mg-7"></div>
+          <div className="text-center mg-7">
+            <button
+              type='button'
+              onClick={(e)=>{
+                setText(e.target.textContent)
+                setActiveBtn('created')
+              }}
+              className={`${activeBtn === 'created' ? activeBtnStyles : notActiveBtnStyles }`}
+            >
+              Created
+            </button>
+            <button
+              type='button'
+              onClick={(e)=>{
+                setText(e.target.textContent)
+                setActiveBtn('saved')
+              }}
+              className={`${activeBtn === 'saved' ? activeBtnStyles : notActiveBtnStyles }`}
+            >
+              Saved
+            </button>
+          </div>
+          <div className="px-2">
+            {pins.le}
+          </div>
         </div>
       </div>
     </div>
