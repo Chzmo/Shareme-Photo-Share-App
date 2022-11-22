7d1b4bde8154167ff7115fc1ba3704a8e8d209ee
Chzmo <chisomomoyo10@gmail.com>
2022-11-22 15:04:29 +0200

    Working on Search input

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,9 @@
-import { data } from 'autoprefixer';
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 
 import {client} from '../client';
 import {feedQuery, searchQuery} from '../utils/data'
 import MasonryLayout from './MasonryLayout';
-import Search from './Search';
 import Spinner from './Spinner'
 
 function Feed() {
