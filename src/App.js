9519b2616fb5c84fd73c788d8dcab05b8240cf50
Chzmo <chisomomoyo10@gmail.com>
2022-11-22 16:03:03 +0200

    redirects

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() =>{
     const user = fetchUser();
     if(!user) navigate('/login');
-  })
+  },[])
   return (
     <Routes>
       <Route path='login' element={<Login/>}/>
