d918397324a25240bb67d0099177a4a34e0dbd44
Chzmo <chisomomoyo10@gmail.com>
2022-11-22 15:53:58 +0200

    Working on Search input

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './index.css';
 import {Routes, Route, useNavigate} from 'react-router-dom'
 import Home from './container/Home';
 import Login from './components/Login';
+import { useEffect } from 'react';
+import { fetchUser } from './utils/fetchUser';
 
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() =>{
+    const user = fetchUser();
+    if(!user) navigate('/login');
+  })
   return (
     <Routes>
       <Route path='login' element={<Login/>}/>
