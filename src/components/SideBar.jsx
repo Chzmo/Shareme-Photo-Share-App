99d1d942ef9bfbdd7c8cee9131a432e58d2b4d74
Chisomo Moyo <56757405+Chzmo@users.noreply.github.com>
2023-04-20 08:02:28 +0200

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -23,7 +23,7 @@ const SideBar = ({user, closedToggle}) =>{
           className='flex px-5 gap-2 my-3 pt-1 w-190 items-center'
           onClick={handleCloseSidebar}
         >
-          <img src={logo} alt="logo" className='w-12 rounded-full'/>
+          <img src={logo} alt="logo" className='w-16 rounded-full'/>
         </Link>
         <div className="flex flex-col gap-2">
           <NavLink
